1bee78f482d29babfc722328bb009fa17dc69f8e
Renato Carvalho <renato.carvalho@direct.one>
2022-01-14 13:56:53 -0300

    feat: criação rotas de autentificação spotify

---
+++ interface/http/spotify-authen.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const spotifyMethods = require('../controllers/spotify_controler');
+
+module.exports = (app) => {
+    router.get('/', (request, response) => {       
+
+        response.redirect(spotifyMethods.getCodeAuthen());
+    });
+
+    router.get('/callback', async (request, response) => {
+        const result = request.query;
+
+        if (result.code) {
+           try {
+                const userData = await spotifyMethods.getDataUser(result.code);
+                response.json(userData)
+           } catch (error) {
+               response.status(error.response.status).json({error: error.message});               
+           }
+        } else {
+            response.status(500).json({ erro: 'Falha no processamento interno'});
+        }  
+    });
+
+    app.use('/spotify', router);
+}
